f61f0eeb095892eb6a6591491b4692130e3615bd
Natalia <natalia.torreira@foundryhq.com>
2017-11-09 20:25:54 +1300

    Library taking care of API requests which require data from a combination of models, not just only one, like building the form JSON structure for a form

---
+++ src/api/js/form_operations.js
@@ -0,0 +1,34 @@
+var form_db_models = require('./db/form_db_models');
+var models = require('./models/index');
+
+module.exports = form_operations;
+
+Form_Operations.getFormJsonStructureById = function getFormJsonStructureById(form_id, callback){
+    //get form structure
+    var form_structure = {}; //this variable will contain the final result, the structure for the form.
+    form_operations.form_db.selectFormById(form_id, function(err, form){
+      if(err){
+        callback(err, err.message);
+      }
+      else{
+        form_structure = {
+          "form": {
+            "vertical_id": form.vertical_id,
+            "form_id": form_id,
+            "form_submission_id": null,
+            "title": form.title,
+            "description": form.description,
+            "type": "user_personal" //not sure what's the purpose of this field, it's not included in the schema
+            },
+            "questions": {
+            }
+          };
+        //get form_questions from the structure
+        //get form questions per each question in the form structure
+
+        //build form_structure combining the results of form
+        //this is gonna be the final result
+        callback(null, form_structure);
+
+      }
+  });
