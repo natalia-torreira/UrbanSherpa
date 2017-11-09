1f7188b70610c24c248d62a5afea9033d4bfc5b4
Natalia <natalia.torreira@foundryhq.com>
2017-11-09 20:23:42 +1300

    refactoring config database file to match it with the sequiliza documentation

--- src/api/config/database.js
+++ src/api/config/database.js
@@ -1,10 +1,16 @@
 module.exports = {
   development: {
+    database: process.env.POSTGRES_DATABASE,
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DATABASE,
     host: process.env.POSTGRES_HOST,
     dialect: "postgres",
     migrationStorageTableName: 'sequelize_meta',
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
   }
 };
